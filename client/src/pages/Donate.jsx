bb80cad68bd830d38c9a5602e27c496685fbf62d
spamprx <cs23btech11012@iith.ac.in>
2024-09-04 19:34:43 +0530

    Added the Donate Page

--- client/src/pages/Donate.jsx
+++ client/src/pages/Donate.jsx
@@ -1,9 +1,15 @@
 function Donate () {
     return (
-        <div className="flex flex-col mx-auto">
-            <h1>In Progress ......</h1>
+        <div className="bg-yellow-50 min-h-screen flex flex-col items-center justify-center space-y-8">
+          <div className="bg-gray-200 w-64 h-64 flex items-center justify-center rounded-lg">
+            <span className="text-lg font-bold">QR Code</span>
+          </div>
+          <div className="bg-gray-200 w-96 p-6 rounded-lg text-center">
+            <p className="text-lg font-bold">Bank account number: XYZ</p>
+            <p className="text-lg font-bold">Phonepe number: XXXXXXXXXX</p>
+          </div>
         </div>
-    )
+      );
 }
 
 export default Donate
