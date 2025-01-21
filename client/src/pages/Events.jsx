1b7d0298016c58d2f330a01042e85400db89c3f7
Tejasri <ep23btech11018@iith.ac.in>
2025-01-21 18:52:15 +0530

    Added prerna drive link

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -21,22 +21,36 @@ const Events = () => {
           </h1>
           <div className="flex flex-col items-center justify-center p-4 my-2 lg:w-3/4 text-md lg:text-xl">
             <p>
-              We invite school students to join us for an exciting and educational event at the Indian Institute of Technology 
-              Hyderabad (IITH) campus on December 17, 2024. Prerna is an event aimed at motivating school students and introducing 
-              them to the world of higher education and innovation. The day includes inspiring speeches by esteemed
-              guests, an engaging campus tour, a fun quiz, cultural performances, and interactive 
-              sessions with IPS and IRS officers. The event concludes with prize distribution. 
-              Join us for a day of discovery and excitement at IITH!
+              We invite school students to join us for an exciting and
+              educational event at the Indian Institute of Technology Hyderabad
+              (IITH) campus on December 17, 2024. Prerna is an event aimed at
+              motivating school students and introducing them to the world of
+              higher education and innovation. The day includes inspiring
+              speeches by esteemed guests, an engaging campus tour, a fun quiz,
+              cultural performances, and interactive sessions with IPS and IRS
+              officers. The event concludes with prize distribution. Join us for
+              a day of discovery and excitement at IITH!
             </p>
-            <a 
-              href="https://docs.google.com/spreadsheets/d/1-us-6_EekcWspf-V-leOp0rO7rSYH5-der_a880Rfa0/edit?gid=351336035#gid=351336035" 
-              target="_blank" 
-              rel="noopener noreferrer" 
-              className="mt-6 mb-4 px-8 py-2 bg-white text-[#556B2F] rounded-lg tracking-wider font-medium 
-                transition-all duration-300 hover:bg-opacity-90 hover:scale-105"
-            >
-              Schedule
-            </a>
+            <div className="flex">
+              <a
+                href="https://drive.google.com/drive/folders/1ZEDJGGUR1ra2FJHFAyyHJHZsGuXI5Sdr"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-6 m-4 px-8 py-2 bg-white text-[#556B2F] rounded-lg tracking-wider font-medium
+                  transition-all duration-300 hover:bg-opacity-90 hover:scale-105"
+              >
+                Gallery
+              </a>
+              <a
+                href="https://docs.google.com/spreadsheets/d/1-us-6_EekcWspf-V-leOp0rO7rSYH5-der_a880Rfa0/edit?gid=351336035#gid=351336035"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-6 mb-4 px-8 py-2 bg-white text-[#556B2F] rounded-lg tracking-wider font-medium
+                  transition-all duration-300 hover:bg-opacity-90 hover:scale-105"
+              >
+                Schedule
+              </a>
+            </div>
           </div>
         </div>
         <div className="w-3/4 h-1/2 bg-[#556B2F] rounded-xl flex flex-col items-center mt-10">
