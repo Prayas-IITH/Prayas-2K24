0c8544cedf21c5dd2102dffe31b75c9724168dc3
rohith-0601 <rohithperugu3@gmail.com>
2025-11-25 15:53:25 +0530

    image size increased in gallery

--- client/src/components/GalleryCard.jsx
+++ client/src/components/GalleryCard.jsx
@@ -6,25 +6,25 @@ function GalleryCard({ title, description, image, index }) {
       </div>
 
       <div
-        className={`flex flex-col md:flex-row w-full mx-auto rounded-2xl h-auto md:h-[45vh] shadow-lg border border-8 border-[#556B2F] ${
+        className={`flex flex-col md:flex-row w-full mx-auto rounded-2xl h-auto md:h-[50vh] shadow-lg border border-8 border-[#556B2F] ${
           index % 2 === 0 ? "bg-[#EDCB5D]" : "bg-[#E5DD57]"
         }`}
       >
-        {/* IMAGE */}
-        <div className="w-full md:w-3/5 h-full flex justify-center items-center p-2">
+        {/* IMAGE SIDE */}
+        <div className="w-full md:w-3/5 h-auto flex justify-center items-center p-2">
           <img
             src={image}
-            className="h-[220px] md:h-[300px] w-auto rounded-2xl object-contain"
+            className="max-h-[300px] md:max-h-[380px] w-auto md:m-4 rounded-2xl object-contain"
           />
         </div>
 
-        {/* TEXT */}
-        <div className="flex flex-col w-full md:w-2/5 p-4 overflow-hidden">
+        {/* TEXT SIDE */}
+        <div className="flex flex-col w-full md:w-2/5 p-4 overflow-hidden justify-center items-center text-center">
           <div className="text-xl md:text-2xl lg:text-2xl font-montserrat mb-2">
             {title}
           </div>
 
-          <div className="text-lg text-gray-700 font-light overflow-auto">
+          <div className="text-lg text-gray-700 font-light">
             {description}
           </div>
         </div>
