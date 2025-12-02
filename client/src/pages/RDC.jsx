4c4a7399a0f0e6b50281c62a4193d353a270c551
rohith-0601 <rohithperugu3@gmail.com>
2025-12-02 20:15:00 +0530

    added rdc redirecting link

--- client/src/pages/RDC.jsx
+++ client/src/pages/RDC.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
 
 function RDC() {
+  const goToRdc = () => {
+    window.open("https://rdc.iith.ac.in/home", "_blank");
+  };
+
   return (
     <div
       style={{
         display: 'flex',
+        flexDirection: 'column',
+        gap: '20px',
         justifyContent: 'center',
         alignItems: 'center',
         height: '50vh',
         textAlign: 'center',
-        fontSize:'3rem'
+        fontSize: '3rem'
       }}
     >
       <h2>Will be updated....</h2>
+
+      <button
+        onClick={goToRdc}
+        style={{
+          padding: '14px 28px',
+          fontSize: '1.3rem',
+          cursor: 'pointer',
+          borderRadius: '10px',
+          border: 'none',
+          backgroundColor: '#628B42',
+          color: 'white',
+          boxShadow: '0 6px 0 #628B10',   // 3D effect
+          transition: 'all 0.1s ease'
+        }}
+        onMouseDown={(e) => {
+          e.target.style.boxShadow = '0 3px 0 #1f7a34';
+          e.target.style.transform = 'translateY(3px)';
+        }}
+        onMouseUp={(e) => {
+          e.target.style.boxShadow = '0 6px 0 #1f7a34';
+          e.target.style.transform = 'translateY(0)';
+        }}
+      >
+        Go to RDC Website
+      </button>
     </div>
   );
 }
